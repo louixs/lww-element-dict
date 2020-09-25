76d6ea7edc0a37578630aef38091423fc0a7023f
louixs <ryuei.sasaki@gmail.com>
2020-09-25 17:19:58 +0800

    added more generative tests

--- test/lww_element/core_property_test.clj
+++ test/lww_element/core_property_test.clj
@@ -20,8 +20,29 @@
              gen-id
              gen/large-integer)))
 
+;; Applying merge is same as appling twice
+;; given the same inputs
+(defspec merge-is-idempotent 100
+  (prop/for-all [d1 gen-dict
+                 d2 gen-dict]
+   (= (lww/merge d1 d2)
+      (lww/merge d1 (lww/merge d1 d2)))))
+
+;; Order of operands / args does not change the results
 (defspec merge-is-commutative 100
   (prop/for-all [d1 gen-dict
                  d2 gen-dict]
     (= (lww/merge d1 d2)
        (lww/merge d2 d1))))
+
+(defn- merge-dicts [d1 d2 d3]
+  (lww/merge d1 (lww/merge d2 d3)))
+
+;; Rearranging the parentheses in an expression will not change the result
+(defspec merge-is-associative 100
+  (prop/for-all [d1 gen-dict
+                 d2 gen-dict
+                 d3 gen-dict]
+    (= (merge-dicts d1 d2 d3)
+       (merge-dicts d2 d1 d3)
+       (merge-dicts d3 d2 d1))))
