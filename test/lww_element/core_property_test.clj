c895ae281d9cf8201513b3ad6b558090649cda45
louixs <ryuei.sasaki@gmail.com>
2020-10-08 15:35:31 +0800

    fix failing generative tests

--- test/lww_element/core_property_test.clj
+++ test/lww_element/core_property_test.clj
@@ -1,18 +1,16 @@
 (ns lww-element.core-property-test
   (:require [lww-element.core :as lww]
-            [clojure.test :as t]
-            [clojure.test.check :as tc]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
-(def gen-map
+(defonce gen-map
   (gen/map gen/keyword gen/string-alphanumeric))
 
-(def gen-id
+(defonce gen-id
   (gen/elements ["#id"]))
 
-(def gen-dict
+(defonce gen-dict
   (gen/fmap (fn [[m id ts]]
               (lww/make-dict m id ts))
             (gen/tuple
@@ -20,23 +18,25 @@
              gen-id
              gen/large-integer)))
 
-;; Applying merge is same as appling twice
+(defonce merge-fn (partial lww/merge {}))
+
+;; Applying merge is same as applying it twice
 ;; given the same inputs
 (defspec merge-is-idempotent 100
   (prop/for-all [d1 gen-dict
                  d2 gen-dict]
-   (= (lww/merge d1 d2)
-      (lww/merge d1 (lww/merge d1 d2)))))
+   (= (merge-fn d1 d2)
+      (merge-fn d1 (merge-fn d1 d2)))))
 
 ;; Order of operands / args does not change the results
 (defspec merge-is-commutative 100
   (prop/for-all [d1 gen-dict
                  d2 gen-dict]
-    (= (lww/merge d1 d2)
-       (lww/merge d2 d1))))
+    (= (merge-fn d1 d2)
+       (merge-fn d2 d1))))
 
 (defn- merge-dicts [d1 d2 d3]
-  (lww/merge d1 (lww/merge d2 d3)))
+  (merge-fn d1 (merge-fn d2 d3)))
 
 ;; Rearranging the parentheses in an expression will not change the result
 (defspec merge-is-associative 100
